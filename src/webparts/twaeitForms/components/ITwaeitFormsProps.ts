18ded3552e5bd34fd92f3c753a166aa269e24c6c
Thomas <github@atheos.cc>
2018-08-31 15:24:08 +0200

    scaffolding

---
+++ src/webparts/twaeitForms/components/ITwaeitFormsProps.ts
@@ -0,0 +1,3 @@
+export interface ITwaeitFormsProps {
+  description: string;
+}
---
+++ src/webparts/twaeitForms/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITwaeitFormsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TwaeitFormsWebPartStrings' {
+  const strings: ITwaeitFormsWebPartStrings;
+  export = strings;
+}
